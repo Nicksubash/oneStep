5207382acac1d847688347fa71e09c03fc90b2b5
Nicksubash <nicksubash9@gmail.com>
2025-01-19 22:56:05 +0900

    Added the Resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import portfolioData from "../data/portfolio.json";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { motion } from 'framer-motion';
 
 export default function About() {
   const {
@@ -30,6 +31,74 @@ export default function About() {
             <section className="text-center">
               <p className="text-sm leading-relaxed">{aboutpara}</p>
             </section>
+            <p className="text-xl text-cyan-600 mt-1 md:mt-9">Click to Download</p>
+            <div style={{display:"flex", justifyContent:"center", gap:"20px"}}>
+            <motion.div
+                        initial={{
+                            backgroundImage:
+                                "linear-gradient(to right, white, white), linear-gradient(0deg, #0DB1D6 , white 40%)"
+                        }}
+                        animate={{
+                            backgroundImage:
+                                "linear-gradient(to right, white, white), linear-gradient(360deg, #0DB1D6, white 40%)"
+                        }}
+                        transition={{
+                            type: "tween",
+                            ease: "linear",
+                            duration: 2,
+                            repeat: Infinity
+                        }}
+                        style={{
+                            border: "2px solid transparent",
+                            borderRadius: "40px",
+                            backgroundClip: "padding-box, border-box",
+                            backgroundOrigin: "padding-box, border-box",
+                            padding: "2px", 
+                            display: "inline-block"
+                        }}
+                    >
+                        <button>
+                        <a href="/subashJP.pdf"
+                        className="text-xl p-5 text-gray-600 hover:text-cyan-700 font-semibold transition-all duration-300 transform hover:scale-110"
+                        download="Resume.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer">履歴書</a>
+                        </button>
+                    </motion.div>
+
+                    <motion.div
+                        initial={{
+                            backgroundImage:
+                                "linear-gradient(to right, white, white), linear-gradient(0deg, #0DB1D6 , white 40%)"
+                        }}
+                        animate={{
+                            backgroundImage:
+                                "linear-gradient(to right, white, white), linear-gradient(-360deg, #0DB1D6, white 40%)"
+                        }}
+                        transition={{
+                            type: "tween",
+                            ease: "linear",
+                            duration: 2,
+                            repeat: Infinity
+                        }}
+                        style={{
+                            border: "2px solid transparent",
+                            borderRadius: "40px",
+                            backgroundClip: "padding-box, border-box",
+                            backgroundOrigin: "padding-box, border-box",
+                            padding: "2px", 
+                            display: "inline-block"
+                        }}
+                    >
+                        <button>
+                        <a href="/EnglishResume.pdf"
+                        className="text-xl p-3 text-gray-600 hover:text-cyan-700 font-semibold transition-all duration-300 transform hover:scale-110"
+                        download="Resume.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer">Resume</a>
+                        </button>
+                    </motion.div>
+                    </div>
           </div>
 
           {/* Right Section - Mobile Style */}
