ae670be264439efb9f274af749dfb6737d83eb5a
Nicksubash <nicksubash9@gmail.com>
2025-07-14 14:16:57 +0900

    updated line link

--- src/components/shared/LineContact.jsx
+++ src/components/shared/LineContact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LineContact = () => {
     return (
         <a
-            href="https://qr-official.line.me/gs/M_038ounou_GW.png?oat_content=qr" 
+            href="https://lin.ee/sf41eZX" 
             target="_blank"
             rel="noopener noreferrer"
             className="fixed left-0 top-1/2  z-50 bg-green-500 p-3 rounded-lg shadow-lg hover:bg-green-600 transition-colors duration-300"
